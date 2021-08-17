8faaaf8fec88621e6bedeb61633876a8dd69561e
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2021-08-17 22:48:10 +0530

    Computing K-Neighbors at predict step

--- knn_impl.py
+++ knn_impl.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
+from future.utils import iteritems
+from sortedcontainers import SortedList
 
 
 class KNN(object):
@@ -8,6 +10,22 @@ class KNN(object):
         self.k = k
 
     def fit(self, X, y):
+        # lazy fit function
         self.X = X
         self.y = y
+        
+    def predict(self, X):
+        y = np.zeros(len(X))
+        for i,x in enumerate(X): # test points
+            sl = SortedList() # stores (distance, class) tuples
+            for j,xt in enumerate(self.X): # training points
+                diff = x - xt
+                d = diff.dot(diff)
+                if len(sl) < self.k:
+                    # don't need to check, just add
+                    sl.add( (d, self.y[j]) )
+                else:
+                    if d < sl[-1][0]:
+                        del sl[-1]
+                        sl.add( (d, self.y[j]) )
         
